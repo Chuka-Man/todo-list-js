f159fb784c1f19271cb51fe9da1fe9783044f154
Danil Chukanov <bastertadorio@gmail.com>
2026-06-08 22:23:34 +0700

    Add todo list functionality

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Happy developing ✨')
+const input = document.querySelector(".todo-input");
+const button = document.querySelector(".todo-button");
+const list = document.querySelector(".todo-list");
+
+button.addEventListener("click", function () {
+    const taskText = input.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    const item = document.createElement("li");
+    item.classList.add("todo-item");
+    item.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Удалить";
+    deleteButton.classList.add("delete-button");
+
+    deleteButton.addEventListener("click", function () {
+        item.remove();
+    });
+
+    item.appendChild(deleteButton);
+    list.appendChild(item);
+
+    input.value = "";
+});
